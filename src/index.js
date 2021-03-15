39f7c03aa046367122f8f4121319bf2cfef5740b
panviktar <panviktar@gmail.com>
2021-03-15 23:56:17 +0300

    feat add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+    let lenghthForStr = str.length;
+    if (lenghthForStr % 2 != 0) return false;
+    return (array.length === 0);
+};
